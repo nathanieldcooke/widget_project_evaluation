ed5d015eacfc4f4a4360b88164d98c64fd957889
nathanieldcooke <nathanield.cooke@gmail.com>
2021-09-25 09:13:17 -0700

    complete phase 2

---
+++ src/Clock.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class Clock extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            time: new Date()
+        };
+    }
+
+    componentDidMount = () => {
+        this.interval = setInterval(this.tick, 1000)
+    }
+
+    componentWillUnmount = () => {
+        clearInterval(this.interval)
+    }
+
+    tick = () => {
+        console.log('hello')
+        this.setState({time: new Date()})
+    }
+    
+    render = () => (
+        <div className='clock-widget'>
+            <div><h1>Clock</h1></div>
+            <div>
+                <h2>{
+                    `${this.state.time.getMonth()}/
+                    ${this.state.time.getDate()}/
+                    ${this.state.time.getFullYear()}
+                    `
+                }</h2>
+            </div>
+            <div>
+                <h2>{
+                    `${this.state.time.getHours()}:
+                    ${this.state.time.getMinutes()}:
+                    ${this.state.time.getSeconds()}
+                    `
+                }</h2>
+            </div>
+        </div>
+    )
+};
+
+export default Clock;
